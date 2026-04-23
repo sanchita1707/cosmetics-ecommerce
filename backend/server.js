639e4eae7b79b2502002e0ccfa75ae258a64e3fd
Sanchita Kamble <sanchita@example.com>
2026-04-23 21:21:15 +0530

    fix server crash + mongo connection

--- backend/server.js
+++ backend/server.js
@@ -3,22 +3,25 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
-const Order = require("./models/Order");
-app.listen(PORT, () => {
-  console.log("Server running on port", PORT);
-});
+
+// Middleware
 app.use(cors());
 app.use(express.json());
-app.use("/api/orders", require("./routes/orderRoutes"));
-// ROUTES
+
+// Routes
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/products", require("./routes/productRoutes"));
 app.use("/api/orders", require("./routes/orderRoutes"));
 app.use("/api/payment", require("./routes/paymentRoutes"));
 app.use("/api/admin", require("./routes/adminRoutes"));
-mongoose.connect("mongodb://127.0.0.1:27017/sanique")
-.then(() => console.log("MongoDB Connected"))
-.catch(err => console.log(err));
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+// MongoDB CONNECT (IMPORTANT FIX)
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
+// SINGLE LISTEN ONLY
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT);
+});
